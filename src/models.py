1a3ccdab72674f53f2cc1ae41a442593e7b15746
julio gutierrez briones <julio.gutierrez@kavak.com>
2022-06-19 20:38:40 -0500

    fix: fixed movie model init

--- src/models.py
+++ src/models.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
+from datetime import datetime
 import os
 
 from dataclasses import dataclass
 from sqlalchemy import create_engine
 
 
-@dataclass(unsafe_hash=True)
 class Movie:
     preference_key = int
     movie_title = str
@@ -14,21 +14,22 @@ class Movie:
     place = int
     vote = int
     link = str
+    create_time: datetime | None = None
 
-    def __repr__(self):
-        return f"<Movie {self.movietitle}>"
-
-    def __eq__(self, other):
-        if not isinstance(other, Movie):
-            return False
-        return other.movietitle == self.movietitle
-
-    def __hash__(self):
-        return hash(self.movietitle)
-
-    def __gt__(self, other):
-        if self.movietitle is None:
-            return False
-        if other.movietitle is None:
-            return True
-        return self.movietitle > other.movietitle
+    def __init__(
+        self,
+        preference_key: int,
+        movie_title: str,
+        rating: float,
+        year: int,
+        place: int,
+        vote: int,
+        link: str,
+    ):
+        self.preference_key = preference_key
+        self.movie_title = movie_title
+        self.rating = rating
+        self.year = year
+        self.place = place
+        self.vote = vote
+        self.link = link
